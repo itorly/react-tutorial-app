b65bea95ae5e7fe5fb4087c058a803b374229c0c
itorly <itorly@163.com>
2025-06-12 18:34:57 +0800

    React.createElement

--- 5.JSX/index.jsx
+++ 5.JSX/index.jsx
@@ -5,25 +5,33 @@ const root = document.querySelector("#root");
 const react_dom_root = createRoot(root);
 
 //  React DOM
-const h1 = React.createElement("h1", {"id": "React DOM"}, "Hello World by using React DOM h1");
+const h1 = React.createElement("h1", {"id": "React DOM"}, "Hello World by using React DOM");
 
 //  JSX
-const p = <p id="JSX and React DOM">Hello World by using JSX and React DOM p</p>;
+const p = <p id="JSX and React DOM">Hello World by using JSX and React DOM</p>;
 
 // In React, each render() call replaces the entire content of the root container.
 
 // Using a div wrapper
-react_dom_root.render(
-    <div>
-        {h1}
-        {p}
-    </div>
-);
+// react_dom_root.render(
+//     <div>
+//         {h1}
+//         {p}
+//     </div>
+// );
 
 // Using React Fragment
+// react_dom_root.render(
+//     <>
+//         {h1}
+//         {p}
+//     </>
+// );
+
+// React.createElement
 react_dom_root.render(
-    <>
-        {h1}
-        {p}
-    </>
+    React.createElement("div", null,
+        React.createElement("h1", {"id": "React DOM"}, "createElement: Hello World by using React DOM"),
+        React.createElement("p", {"id": "JSX and React DOM"}, "createElement: Hello World by using JSX and React DOM")
+    )
 );
